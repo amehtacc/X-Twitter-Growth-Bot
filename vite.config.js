773532d3f1579fab74af2a5d8fbc611f2351754e
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-04-07 22:39:24 +0530

    AI Auto Tweet

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'), // updated from index.html
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    target: 'esnext',
+  },
 })
