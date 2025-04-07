2f0610a08a08d60c8f1f1e41234fb07d87b206cc
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-04-07 22:32:03 +0530

    Background service worker

---
+++ src/background/background.js
@@ -0,0 +1,15 @@
+// src/background/background.js
+console.log("✅ Background service worker loaded");
+
+import { toggleAutoTweeting } from "../scripts/auto_tweet_manager.js";
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  console.log("✅ Background received:", msg);
+
+  if (msg.type === "TOGGLE_AUTO_TWEET") {
+    toggleAutoTweeting();
+    sendResponse({ status: "Auto tweet toggled" });
+  }
+
+  return true;
+});
